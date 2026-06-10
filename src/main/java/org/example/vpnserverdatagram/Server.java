c7e645d04aa61cf2d88c15759dd48d8e3e26711b
Vladimir <v.dementev@bftcom.com>
2026-06-11 00:58:53 +0700

    add ping test

--- src/main/java/org/example/vpnserverdatagram/Server.java
+++ src/main/java/org/example/vpnserverdatagram/Server.java
@@ -7,56 +7,29 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.SocketException;
-import java.nio.charset.StandardCharsets;
 
 @Service
 public class Server {
 
     @EventListener(ApplicationReadyEvent.class)
-    public void start() {
-        int port = 51888;
+    public void start() throws IOException {
+        DatagramSocket socket = new DatagramSocket(51888);
 
-        try (DatagramSocket socket = new DatagramSocket(port)) {
-            System.out.println("Server started on port " + port);
+        System.out.println("UDP echo server started on port 51888");
 
-            byte[] buffer = new byte[2048];
+        byte[] buffer = new byte[2048];
 
-            while (true) {
-                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
-                socket.receive(request);
+        while (true) {
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 
-                String message = new String(
-                        request.getData(),
-                        request.getOffset(),
-                        request.getLength(),
-                        StandardCharsets.UTF_8
-                );
+            socket.receive(packet);
 
-                System.out.println("Received from "
-                        + request.getAddress().getHostAddress()
-                        + ":"
-                        + request.getPort()
-                        + " -> " + message);
-
-                if ("PING".equals(message)) {
-                    byte[] responseData = "PONG".getBytes(StandardCharsets.UTF_8);
-
-                    DatagramPacket response = new DatagramPacket(
-                            responseData,
-                            responseData.length,
-                            request.getAddress(),
-                            request.getPort()
-                    );
-
-                    socket.send(response);
-                    System.out.println("Sent PONG");
-                }
-            }
-        } catch (SocketException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            socket.send(new DatagramPacket(
+                    packet.getData(),
+                    packet.getLength(),
+                    packet.getAddress(),
+                    packet.getPort()
+            ));
         }
     }
 
