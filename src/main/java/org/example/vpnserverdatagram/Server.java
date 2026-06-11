5bf39372449896b9c5cb5b5d5f67c7e08a34e1db
Vladimir <v.dementev@bftcom.com>
2026-06-11 22:05:33 +0700

    add ExecutorService

--- src/main/java/org/example/vpnserverdatagram/Server.java
+++ src/main/java/org/example/vpnserverdatagram/Server.java
@@ -7,36 +7,69 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static java.util.Objects.isNull;
 
 @Service
 public class Server {
 
     @EventListener(ApplicationReadyEvent.class)
-    public void start() throws IOException {
-        DatagramSocket socket = new DatagramSocket(51888);
-
-        byte[] buffer = new byte[1024];
-
+    public void start() {
+        ExecutorService sendPool = Executors.newFixedThreadPool(8);
+        DatagramSocket socket = getSocket();
         System.out.println("Server started on UDP port 51888");
 
-        while (true) {
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-            socket.receive(packet);
+        while (!isNull(socket)) {
+            DatagramPacket packet = getRequestPacket();
 
-            String text = new String(packet.getData(), 0, packet.getLength());
+            byte[] data = receive(socket, packet);
 
-            System.out.println("Received: " + text);
-
-            byte[] response = "PONG".getBytes();
-
-            DatagramPacket responsePacket = new DatagramPacket(
-                    response,
-                    response.length,
-                    packet.getAddress(),
-                    packet.getPort()
-            );
-
-            socket.send(responsePacket);
+            sendPool.execute(() -> send(socket, packet, data));
         }
+    }
+
+    private DatagramPacket getRequestPacket() {
+        byte[] buffer = new byte[1024];
+        return new DatagramPacket(buffer, buffer.length);
+    }
+
+    private DatagramSocket getSocket() {
+        try {
+            return new DatagramSocket(51888);
+        } catch (SocketException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    private byte[] receive(DatagramSocket socket, DatagramPacket packet) {
+        try {
+            socket.receive(packet);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        String text = new String(packet.getData(), 0, packet.getLength());
+        System.out.println("Received: " + text);
+        return packet.getData();
+    }
+
+    private void send(DatagramSocket socket, DatagramPacket packet, byte[] response) {
+        DatagramPacket responsePacket = getResponsePacket(response,  packet);
+        try {
+            socket.send(responsePacket);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private DatagramPacket getResponsePacket(byte[] response, DatagramPacket packet) {
+        return new DatagramPacket(
+                response,
+                response.length,
+                packet.getAddress(),
+                packet.getPort());
     }
 }
